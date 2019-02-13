4b62737c3c5941a677a52399c1d30b2013fd7b30
DaemonSpelledWrong <damongself@gmail.com>
2019-02-12 20:38:33 -0500

    Deleted commented text at the top

--- deepcomparison.js
+++ deepcomparison.js
@@ -1,13 +1,3 @@
-/*
-- Write a function, deepEqual, that takes two values and returns true only if 
-- they are the same value or are objects with the same properties whose values 
-- are also equal when compared with a recursive call to deepEqual.
-- To find out whether to compare two things by identity (use the === operator for that)
-- or by looking at their properties, you can use the typeof operator.
-- If it produces "object" for both values, you should do a deep comparison. 
-- But you have to take one silly exception into account: by a historical
-- accident, typeof null also produces "object".
-*/
 
 function deepEqual(x, y) {
     if(typeof x == "object" && x != null && typeof y == "object" && y != null) {
